514184d7c73d1f311cf708394c032529a3f30448
ygstudio-game <yadnyeshsunilborole@gmail.com>
2025-07-22 17:44:36 +0530

    file rename

--- server/routes/protected.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const protect = require('../middleware/authMiddleware');
-
-router.get('/dashboard', protect, (req, res) => {
-  res.json({ msg: `Welcome, user ${req.user.id}` });
-});
-
-module.exports = router;
